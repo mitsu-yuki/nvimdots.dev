ba1bc96327738b8b293b356e5cbdcff699733e4e
Mitsu <tech.mitsu.yuki@gmail.com>
2025-01-23 17:15:12 +0900

    fix: adjust lspconfig for Neovim

--- lua/extensions/lsp.lua
+++ lua/extensions/lsp.lua
@@ -1,6 +1,11 @@
+---@module "lazy"
+---@type LazySpec[]
 return {
-  "neovim/nvim-lspconfig",
-  lazy = true,
-  -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
-  event = { "BufReadPre", "BufNewFile" },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
+    event = { "BufReadPre", "BufNewFile" },
+    config = require("configs.nvim-lspconfig")
+  },
 }
