18b11ade0f665feac3423d1231c559cedb0bca77
Mitsu <tech.mitsu.yuki@gmail.com>
2025-03-05 19:03:55 +0900

    refactor(lua/extensions): update completion module for cleaner organization

--- lua/extensions/completion.lua
+++ lua/extensions/completion.lua
@@ -14,16 +14,4 @@ return {
   -- snippet engine
   { "L3MON4D3/LuaSnip" },
   { "saadparwaiz1/cmp_luasnip" },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = true,
-    -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
-    event = { "BufReadPre", "BufNewFile" },
-    config = require("configs.nvim-lspconfig"),
-  },
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = require("configs.nvim-lint"),
-  },
 }
---
+++ lua/extensions/lint.lua
@@ -0,0 +1,16 @@
+---@module "lazy"
+---@type LazySpec[]
+return {
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
+    event = { "BufReadPre", "BufNewFile" },
+    config = require("configs.nvim-lspconfig"),
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = require("configs.nvim-lint"),
+  },
+}
