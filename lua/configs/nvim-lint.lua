1e221f9d26966f313d4f9022441c155f074f9e02
Mitsu <tech.mitsu.yuki@gmail.com>
2025-03-06 23:31:01 +0900

    style: fix format

--- lua/configs/nvim-lint.lua
+++ lua/configs/nvim-lint.lua
@@ -9,9 +9,9 @@ return function()
     sh = shellcheck,
     zsh = shellcheck,
   }
-  vim.api.nvim_create_autocmd({"BufReadPost", "InsertLeave"}, {
+  vim.api.nvim_create_autocmd({ "BufReadPost", "InsertLeave" }, {
     callback = function()
       lint.try_lint()
-    end
+    end,
   })
 end
