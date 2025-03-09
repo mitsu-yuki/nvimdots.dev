39c579e5fb20e340b1a03c3c6f68232e58919ec3
Mitsu <tech.mitsu.yuki@gmail.com>
2025-03-10 00:08:21 +0900

    feat: add schemastore.nvim

--- lua/extensions/lint.lua
+++ lua/extensions/lint.lua
@@ -7,6 +7,9 @@ return {
     -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
     event = { "BufReadPre", "BufNewFile" },
     config = require("configs.nvim-lspconfig"),
+    dependencies = {
+      { "b0o/SchemaStore.nvim" },
+    },
   },
   {
     "mfussenegger/nvim-lint",
