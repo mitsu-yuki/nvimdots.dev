8e9a71f33f20c0c464a5dda83de054ea973fb04e
Mitsu <tech.mitsu.yuki@gmail.com>
2025-04-21 15:38:01 +0900

    feat: add <C-V> mapping

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,4 +15,9 @@ vim.keymap.set("x", "p", "P")
 vim.keymap.set("i", "<Left>", "<C-G>U<Left>")
 vim.keymap.set("i", "<Right>", "<C-G>U<Right>")
 
+-- SQL入力中に発動するオムニ補完を<C-C>から<C-J>に変更
 vim.g.ftplugin_sql_omni_key = "<C-J>"
+
+-- インサートモード中に<C-V>されても無視する
+vim.keymap.set("i", "<C-V>", "")
+vim.keymap.set("i", "<C-V><C-V>", "<C-V>")
