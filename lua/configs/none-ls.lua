aa61d8dab3fb5f2062df7a5542b05b3b3b269a66
Mitsu <tech.mitsu.yuki@gmail.com>
2025-06-30 14:29:50 +0900

    fix: none-ls setting

--- lua/configs/none-ls.lua
+++ lua/configs/none-ls.lua
@@ -2,27 +2,20 @@ return function()
   local null_ls = require("null-ls")
   local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
-
-  local textlint = diagnostics.textlint.with({
-    command = function()
-      local local_binary = vim.fn.fnamemodify("./node_modules/.bin/textlint", ":p")
-      return vim.loop.fs_stat(local_binary) and local_binary or "textlint"
-    end,
-  })
+  local code_actions = null_ls.builtins.code_actions
 
   null_ls.setup({
     sources = {
       diagnostics.ansiblelint,
-      diagnostics.shellcheck,
-      diagnostics.jsonlint,
-      diagnostics.perlcritic,
       diagnostics.sqruff,
-      textlint,
+      diagnostics.textlint,
+      formatting.textlint,
       formatting.stylua,
       formatting.shfmt,
       formatting.goimports,
       formatting.gofmt,
       formatting.sqruff,
+      code_actions.textlint,
     },
   })
 
