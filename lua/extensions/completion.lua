a33307cc99186f6ee5312a0d45b28d4016035db4
Mitsu <tech.mitsu.yuki@gmail.com>
2025-08-26 16:48:20 +0900

    format: auto format for formatter

--- lua/extensions/completion.lua
+++ lua/extensions/completion.lua
@@ -36,11 +36,11 @@ return {
     config = require("configs.nvim-cmp"),
   },
   -- nvim-cmp dependencies
-  { "hrsh7th/cmp-nvim-lsp", event = { "InsertEnter" } },
-  { "hrsh7th/cmp-buffer", event = { "InsertEnter" } },
-  { "hrsh7th/cmp-path", event = { "InsertEnter" } },
-  { "hrsh7th/cmp-cmdline", event = { "CmdlineEnter" } },
-  { "onsails/lspkind.nvim", event = { "InsertEnter" } },
+  { "hrsh7th/cmp-nvim-lsp",         event = { "InsertEnter" } },
+  { "hrsh7th/cmp-buffer",           event = { "InsertEnter" } },
+  { "hrsh7th/cmp-path",             event = { "InsertEnter" } },
+  { "hrsh7th/cmp-cmdline",          event = { "CmdlineEnter" } },
+  { "onsails/lspkind.nvim",         event = { "InsertEnter" } },
   -- snippet engine
   { "L3MON4D3/LuaSnip" },
   { "rafamadriz/friendly-snippets", event = { "InsertEnter", "CmdlineEnter" } },
