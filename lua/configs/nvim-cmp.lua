6c014573c7717a7e5adb569abe10ca5bd1c03d5e
Mitsu <tech.mitsu.yuki@gmail.com>
2025-08-26 16:47:53 +0900

    wip: add luasnip.jump

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -33,7 +33,8 @@ return function()
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          luasnip.expand_or_jump()
+          -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
         else
           fallback()
         end
@@ -42,7 +43,8 @@ return function()
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
-          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          luasnip.jump(-1)
+          -- vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
         else
           fallback()
         end
