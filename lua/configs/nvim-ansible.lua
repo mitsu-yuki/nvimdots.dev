ca87d2b6365983230b82db9f20b7ef2122a9bb2a
Mitsu <127361119+mitsu-yuki@users.noreply.github.com>
2026-01-25 17:03:20 +0900

    feat: add nvim-ansible

---
+++ lua/configs/nvim-ansible.lua
@@ -0,0 +1,3 @@
+return function()
+  require("ansible")
+end
--- lua/extensions/lint.lua
+++ lua/extensions/lint.lua
@@ -23,6 +23,11 @@ return {
     config = require("configs.lspsaga"),
   },
   {
+    "mfussenegger/nvim-ansible",
+    ft = { "yaml" },
+    config = require("configs.nvim-ansible"),
+  },
+  {
     "neovim/nvim-lspconfig",
     lazy = true,
     -- ref: https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
