36a22bb292258f5ea74d421228fb567df7bd2ac7
Mitsu <tech.mitsu.yuki@gmail.com>
2026-03-30 16:49:37 +0900

    fix: typo-lsp config

--- after/lsp/typos_lsp.lua
+++ after/lsp/typos_lsp.lua
@@ -1,9 +1,6 @@
 ---@type vim.lsp.Config
 return {
   init_options = {
-    {
-      config = vim.env.XDG_CONFIG_HOME .. "/typos-lsp/typos.toml",
-      diagnosticSeverity = "Error",
-    },
+    config = vim.env.XDG_CONFIG_HOME .. "/typos-lsp/typos.toml",
   },
 }
