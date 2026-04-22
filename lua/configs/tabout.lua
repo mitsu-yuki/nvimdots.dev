f2cc5632dc978f8e2652527ed72fddc5e6197951
Mitsu <tech.mitsu.yuki@gmail.com>
2026-04-22 11:03:58 +0900

    feat: use tabout default setting

--- lua/configs/tabout.lua
+++ lua/configs/tabout.lua
@@ -1,13 +1,4 @@
 return function()
   ---@type TaboutOptions
-  require("tabout").setup({
-    tabkey = "<Tab>",
-    backwards_tabkey = "<S-Tab>",
-    act_as_tab = true,
-    act_as_shift_tab = false,
-    completion = true,
-    default_tab = "<C-t>",
-    default_shift_tab = "<C-d>",
-    enable_backwards = true,
-  })
+  require("tabout").setup({})
 end
